5d1715d09931fee7ab376549dcb7c35217b3d20c
nullgemm <nullgemm@mailbox.org>
2023-10-29 18:27:21 +0100

    remove temporary branch checkout in ci scripts

--- ci/cursoryx_image_appkit_osxcross/scripts/build_appkit.sh
+++ ci/cursoryx_image_appkit_osxcross/scripts/build_appkit.sh
@@ -3,9 +3,6 @@
 git clone https://github.com/nullgemm/cursoryx.git
 cd ./cursoryx || exit
 
-# TODO remove
-git checkout next
-
 # test build
 export PATH=/scripts/sdk/target/bin/:/bin:/sbin:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/scripts/sdk/target/lib/
--- ci/cursoryx_image_win_mingw/scripts/build_win.sh
+++ ci/cursoryx_image_win_mingw/scripts/build_win.sh
@@ -3,8 +3,5 @@
 git clone https://github.com/nullgemm/cursoryx.git
 cd ./cursoryx || exit
 
-# TODO remove
-git checkout next
-
 # test build
 ./make/scripts/build.sh "$@"
